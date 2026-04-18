f5bd998c104bc9d0e4491d6ffc7a1d3f3421f416
viveksangadiya <70437153+viveksangadiya@users.noreply.github.com>
2026-04-18 15:06:26 +0530

    bd

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+// });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
+await pool.query('SET search_path TO public');
 pool.on('connect', () => {
   if (process.env.NODE_ENV !== 'test') {
     console.log('📦 Connected to PostgreSQL');
