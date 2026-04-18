5f57504b189bda234c37659233656936e757fb48
viveksangadiya <70437153+viveksangadiya@users.noreply.github.com>
2026-04-18 15:09:47 +0530

    derfd

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,17 +12,22 @@ const allowedOrigins = [
 if (process.env.FRONTEND_URL) {
   allowedOrigins.push(process.env.FRONTEND_URL);
 }
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
+  origin: (origin, callback) => {
+    callback(null, true); // allow all
   },
   credentials: true
 }));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("CORS not allowed"));
+//     }
+//   },
+//   credentials: true
+// }));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
